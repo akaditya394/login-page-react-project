ce43a274d7d6b759a2432930d577e2e5aae9cc0f
Aditya <akaditya394@gmail.com>
2022-02-04 20:34:38 +0530

    cleanup function added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,9 +11,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const indentifier = setTimeout(() => {
+      console.log("checking form validity...");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(indentifier);
+    };
   }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
